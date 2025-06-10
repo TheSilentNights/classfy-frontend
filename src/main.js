6eae2b1d13e70280ca36246884f0298d0bc96508
TheSilentNights <97817549+TheSilentNights@users.noreply.github.com>
2025-06-10 22:58:37 +0800

    start

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import "element-plus/dist/index.css"
+import ElementPlus from "element-plus"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
