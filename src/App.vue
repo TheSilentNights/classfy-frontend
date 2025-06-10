c5797f7b6b987c88f8744d67df779dde83542f3c
TheSilentNights <97817549+TheSilentNights@users.noreply.github.com>
2025-06-10 23:10:07 +0800

    add Layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
-
 <template>
-  
+  <div class="main">
+    <Layout/>
+  </div>
 </template>
 
 <script setup>
+import Layout from './components/Layout.vue';
+
 
 </script>
 
 <style scoped>
-
+.main{
+    height: 100%;
+    width: 100%;
+}
 </style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.el-container{
+    width: 100%;
+    height: 100%;
+}
+.el_header{
+    width: 100%;
+    height: 100%;
+}
+.common-layout{
+    height: 100%;
+    width: 100%;
+}
+</style>
