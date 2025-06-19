57d1921bd9964934fde34760c56ae8ca8e4ec395
TheSilentNights <97817549+TheSilentNights@users.noreply.github.com>
2025-06-19 22:11:14 +0800

    更改

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,13 +1,19 @@
 <template>
-	<div class="common-layout">
-		<el-container>
-			<el-header><MainHeader /></el-header>
-			<el-container>
-				<el-aside width="300px"><MainAside /></el-aside>
-				<el-main><MainMain /></el-main>
-			</el-container>
-		</el-container>
-	</div>
+    <div class="common-layout">
+        <el-container>
+            <el-header>
+                <MainHeader/>
+            </el-header>
+            <el-container>
+                <el-aside width="300px">
+                    <MainAside/>
+                </el-aside>
+                <el-main>
+                    <MainMain/>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script setup>
@@ -18,23 +24,27 @@ import MainMain from './MainMain.vue'
 
 <style scoped>
 .el-container {
-	width: 100%;
-	height: 100%;
+    width: 100%;
+    height: 100%;
 }
+
 .el-header {
-	padding: 0;
-	margin-bottom: 20px;
+    padding: 0;
+    margin-bottom: 20px;
 }
+
 .el-aside {
-	overflow: hidden;
-	height: 100%;
+    overflow: hidden;
+    height: 100%;
 }
+
 .el-main {
-	padding: 0;
-	margin-left: 10px;
+    padding: 0;
+    margin-left: 10px;
 }
+
 .common-layout {
-	height: 100%;
-	width: 100%;
+    height: 100%;
+    width: 100%;
 }
 </style>
